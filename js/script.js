28eea252877cb51c45585175b56d80960956dbd6
carmelopalumbo <carmelopalumbo@outlook.it>
2022-10-14 16:11:18 +0200

    add-es2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 // js
 
+
+// es1
 const mailList = ['ciao@gmail.com', 'buonasera@gmail.com', 'arrivederci@outlook.it', 'boolean@hotmail.it', 'classe75@gmail.com', 'zoomclasse75@live.it'];
 const inputBtn = document.getElementById('verifica');
 let isIntolist = false;
@@ -10,11 +12,51 @@ inputBtn.addEventListener('click', function(){
     for( let i = 0; i < mailList.length; i++){
         if(inputMail.toLowerCase() === mailList[i]){
             isIntolist = true;
+            break;
         }
     }
+    console.log(isIntolist);
     if(isIntolist){
         mailOutput.innerHTML = 'Hai accesso a questa lista mail.';
     }else{
         mailOutput.innerHTML = 'NON hai accesso a questa lista. Inserisci una mail valida.';
     }
+})
+
+
+
+
+// es2
+const minDado = 1;
+const maxDado = 6
+const numBtn = document.getElementById('actiondadi');
+const container = document.querySelector('.finaldadi');
+let outputUser;
+let outputCpu;
+let outputResult;
+let numUser = 0;
+let numCpu = 0;
+
+numBtn.addEventListener('click', function(){
+    numUser = Math.floor(Math.random() * (maxDado - minDado + 1) + minDado);
+    numCpu = Math.floor(Math.random() * (maxDado - minDado + 1) + minDado);
+    console.log(numUser);
+    console.log(numCpu);
+    outputUser = document.createElement('div');
+    outputCpu = document.createElement('div');
+    outputUser.innerHTML = `Il tuo numero é ${numUser}`;
+    outputCpu.innerHTML = `Il numero della cpu é ${numCpu}`;
+    container.append(outputUser);
+    container.append(outputCpu);
+
+    outputResult = document.createElement('div');
+    if(numUser > numCpu){
+        outputResult = 'HAI VINTO!';
+    }else if(numCpu > numUser ){
+        outputResult = 'Hai perso :( riprova!';
+    }else{
+        outputResult = 'PAREGGIO!';
+    }
+
+    container.append(outputResult);
 })
