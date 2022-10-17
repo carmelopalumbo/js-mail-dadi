4e215cf28ccdd0d1e4db86fa821b95926c015f3a
carmelopalumbo <carmelopalumbo@outlook.it>
2022-10-17 09:48:03 +0200

    fix-input-mail

--- js/script.js
+++ js/script.js
@@ -5,22 +5,21 @@
 const mailList = ['ciao@gmail.com', 'buonasera@gmail.com', 'arrivederci@outlook.it', 'boolean@hotmail.it', 'classe75@gmail.com', 'zoomclasse75@live.it'];
 const inputBtn = document.getElementById('verifica');
 let isIntolist = false;
-let inputMail = document.getElementById('mymail').value;
 let mailOutput = document.getElementById('accesso');
 
 inputBtn.addEventListener('click', function(){
+    let inputMail = document.getElementById('mymail').value;
     for( let i = 0; i < mailList.length; i++){
         if(inputMail.toLowerCase() === mailList[i]){
             isIntolist = true;
-            break;
         }
     }
-    console.log(isIntolist);
     if(isIntolist){
         mailOutput.innerHTML = 'Hai accesso a questa lista mail.';
     }else{
         mailOutput.innerHTML = 'NON hai accesso a questa lista. Inserisci una mail valida.';
     }
+    isIntolist = false;
 })
 
 
